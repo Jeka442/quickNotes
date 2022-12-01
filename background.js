3ea97c267d378a06e9a48c47ad792869ae5bfc71
Jeka <jeka.gudilov@allgreen.app>
2022-12-01 23:26:48 +0200

    now we can save to notes

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 
 function PasteToElement(info, tab, text) {
-    chrome.tabs.sendMessage(tab.id, { message: "pasteToInput", value:text }, { frameId: info.frameId });
+    chrome.tabs.sendMessage(tab.id, { message: "pasteToInput", value: text }, { frameId: info.frameId });
 }
 
 chrome.storage.local.onChanged.addListener(async () => {
-    await chrome.contextMenus.removeAll();
-    await chrome.contextMenus.create({
+    await chrome.contextMenus.remove("QuickNotes")
+    chrome.contextMenus.create({
         id: "QuickNotes",
         title: "QuickPaste",
         contexts: ["editable"]
@@ -23,8 +23,39 @@ chrome.storage.local.onChanged.addListener(async () => {
         }
     }
 })
+
+
+const GetUuid = () => {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (
+            c ^
+            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+    );
+};
+
+const addValueAsync = async (value) => {
+    const list = await chrome.storage.local.get("Notes");
+    let newList = [];
+    if (list.Notes) newList = [...list.Notes];
+    newList.push({ id: GetUuid(), value: value });
+    chrome.storage.local.set({ Notes: newList });
+}
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    const id = info.menuItemId;
-    const text = id.substring(id.indexOf("-") + 1);
-    PasteToElement(info, tab, text)
+    if (info.menuItemId === "QuickNotes") return;
+    if (info.menuItemId === "QuickNotesAdd") {
+        const value = info.selectionText;
+        if (value) addValueAsync(value);
+    } else {
+        const id = info.menuItemId;
+        const text = id.substring(id.indexOf("-") + 1);
+        PasteToElement(info, tab, text)
+    }
 });
+
+
+chrome.contextMenus.create({
+    id: "QuickNotesAdd",
+    title: "Save as Note",
+    contexts: ["selection"]
+})
