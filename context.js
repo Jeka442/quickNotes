137de507ee3d67a3900dc206a197ffe1d64b343f
Jeka <jeka.gudilov@allgreen.app>
2022-12-02 15:29:40 +0200

    bug fix

--- context.js
+++ context.js
@@ -11,7 +11,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         const tagName = elm.tagName.toLowerCase();
         if (tagName == "input" || tagName == "textarea" || tagName == "select") {
             let changeEvent = new Event("change", { bubbles: true, cancelable: true, composed: true });
-            elm.setAttribute("value", request.value);
+            elm.value = request.value;
             elm.innerText = request.value;
             elm.dispatchEvent(changeEvent);
         } else {
