2b7c74a03c4089759f96c49655733b8d8866c0a6
Jeka <jeka.gudilov@allgreen.app>
2022-12-02 04:06:45 +0200

    done

--- background.js
+++ background.js
@@ -26,7 +26,7 @@ const addContextMenu = async () => {
         let index = 0;
         for (let note of Notes.Notes) {
             chrome.contextMenus.create({
-                id: `${index++}-${note.value}`,
+                id: `${index++}-${note.id}`,
                 parentId: "QuickNotes",
                 title: `${note.value.substring(0, 7)}...`,
                 contexts: ["all"]
@@ -59,8 +59,11 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
         if (value) addValueAsync(value);
     } else {
         const id = info.menuItemId;
-        const text = id.substring(id.indexOf("-") + 1);
-        PasteToElement(info, tab, text)
+        const noteId = id.substring(id.indexOf("-") + 1);
+        chrome.storage.local.get("Notes").then(Notes => {
+            PasteToElement(info, tab, Notes.Notes.find(n => n.id === noteId).value)
+        })
+        // PasteToElement(info, tab, text)
     }
 });
 
